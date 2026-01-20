47e0bc14d7049e659bb51a65d79967031f5c9183
siddev99 <ghostrider949133@gmail.com>
2026-01-20 11:30:23 +0530

    Added configuration from gateway to product

---
+++ product-service/src/main/java/com/cartverse/product_service/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.cartverse.product_service.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @GetMapping("/getMessage")
+    public String message(){
+        return "HI";
+    }
+}
